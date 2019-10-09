24b50b1db509a5f3fbcd91d66e890ec6688f571c
Filip Sheronau <Filsheronov@gmail.com>
2019-10-09 20:48:50 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  // your solution  
+  let a = first.split('').reverse();
+  let b = second.split('').reverse();
+  let arr = [];
+  for (var i = 0; i < a.length; i++) {
+    let mem = 0;
+    for (var j = 0; j < b.length; j++) {
+      let res1 = a[i]*b[j]+mem;      
+      if (arr[i+j]) {
+        let res2 = arr[i+j] + res1
+        arr[i+j] = res2 % 10;
+        mem = Math.floor(res2 / 10);
+      }
+      else {        
+        arr[i+j] = res1 % 10;
+        mem = Math.floor(res1 / 10);
+      }
+      if (j == b.length-1 && mem > 0) {
+        arr[i+j+1] = mem;
+      }     
+    }
+  }  
+  return arr.reverse().join('');
 }
